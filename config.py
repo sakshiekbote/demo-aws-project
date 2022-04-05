f84cecc2b3dbf548003f9837985a68a1c426a3f4
Sakshi Ekbote <88487185+sakshiekbote@users.noreply.github.com>
2022-04-05 11:25:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-customhost = "mydatabase.ci4td1u42qon.us-east-1.rds.amazonaws.com"
+customhost = "mydatabase.c283ngfe3exb.us-east-1.rds.amazonaws.com"
 customuser = "admin"
 custompass = "admin123"
 customdb = "mydatabase"
-custombucket = "my-project2-bucket"
+custombucket = "myproject-2-bucket"
 customregion = "us-east-1"
